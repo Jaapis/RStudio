b19b560cc910c56b629e7e3cd7fa10d48db57a4b
Jaapis <rodrigo.horie@hotmail.com>
2019-06-04 19:19:53 -0700

    Aula 01

---
+++ Aulas/01 - Basico.R
@@ -0,0 +1,17 @@
+# Get do Working Directory
+getwd()
+# Set do Working Directory
+setwd('/home/jaapis/Documents/GitHub/RStudio/Aulas')
+
+# Instala ggplot2
+install.packages('ggplot2', dependencies = TRUE)
+
+# Chama a bilbioteca ggplot2
+library(ggplot2)
+
+# Instala RColorBrewer
+install.packages('RColorBrewer', dependencies = TRUE)
+
+# Chama a biblioteca RColorBrewer
+library(RColorBrewer)
+
