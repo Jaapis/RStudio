3e48f2d3e349ec22dd88772360084ccd9ebb0db0
Jaapis <rodrigo.horie@hotmail.com>
2019-06-05 13:18:41 -0700

    Adicionada Aula 06 Completa

--- Aulas/06 - Facebook.R
+++ Aulas/06 - Facebook.R
@@ -103,31 +103,70 @@ ggplot(aes(x = www_likes_received, y = likes_received), data = pf) +
 with(subset(pf), cor.test(pf$www_likes_received, pf$likes_received))
 
 # Instala alr3
+# Rodar sudo apt-get install r-cran-car
+
 install.packages("alr3", repos="http://R-Forge.R-project.org")
 library(alr3)
 data(Mitchell)
 
-install.packages("car")
-library('car')
+?Mitchell
 
+head(Mitchell)
 
+ggplot(aes(x = (Month%%12), y = Temp), data = Mitchell) + 
+  geom_point(color = 'orange') +
+  scale_x_discrete(breaks = seq(0, 203, 12))
 
+cor.test(Mitchell$Month, Mitchell$Temp)
 
+# Entendendo o ruído
+ggplot(aes(x = age, y = friend_count_mean),
+       data = pf.fc_by_age) +
+  geom_line()
 
+head(pf.fc_by_age, 10)
 
+pf.fc_by_age[17:19, ]
 
+pf$age_with_months <- pf$age + (12 - pf$dob_month)/12
 
+# Média de idade com meses
+pf.fc_by_age_months <- pf %>%
+  group_by(age_with_months) %>%
+  summarise(friend_count_mean = mean(friend_count),
+            friend_count_median = median(friend_count),
+            n = n()) %>%
+  arrange(age_with_months)
 
+head(pf.fc_by_age_months)
 
+# Solução alternativa
+age_with_months_groups <- group_by(pf, age_with_months)
+pf.fc_by_age_months2 <- summarise(age_with_months_groups,
+                                  friend_count_mean = mean(friend_count),
+                                  friend_count_median = median(friend_count),
+                                  n = n())
+pf.fc_by_age_months2 <- arrange(pf.fc_by_age_months2, age_with_months)
 
+head(pf.fc_by_age_months2)
 
+# Ruído em média condicional
+p1 <- ggplot(aes(x = age, y = friend_count_mean),
+             data = subset(pf.fc_by_age, age <71)) +
+  geom_line() +
+  geom_smooth()
 
+p2 <- ggplot(aes(x = age_with_months, y = friend_count_mean), 
+          data = subset(pf.fc_by_age_months, age_with_months < 71)) + 
+  geom_line() + 
+  geom_smooth()
 
+p3 <- ggplot(aes(x = round(age/5)*5, y = friend_count), 
+             data = subset(pf, age < 71)) +
+  geom_line(stat = 'summary', fun.y = mean)
 
-
-
-
-
+library(gridExtra)
+grid.arrange(p2, p1, p2, ncol = 1)
 
 
 
